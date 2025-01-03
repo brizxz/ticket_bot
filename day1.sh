e1d9e0288702f1bdd25bd3847255ee1dfac7b76c
brizxz <yussp6969@gmail.com>
2025-01-03 18:42:05 +0800

    update files

---
+++ day1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python chrome_tixcraft.py --input settings6.json &
+python chrome_tixcraft.py --input settings7.json &
+python chrome_tixcraft.py --input settings8.json &
+python chrome_tixcraft.py --input settings9.json &
